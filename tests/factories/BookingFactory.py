ce5440e4b635312f4b842cc308f1b624d1aabac4
Menchcruts <joelarnar@gmail.com>
2025-03-31 13:40:06 +0000

    Lagaði bug í Booking og BookingFactory og breytti methods í BookingService í static methods.

--- tests/factories/BookingFactory.py
+++ tests/factories/BookingFactory.py
@@ -1,9 +1,9 @@
 import factory, faker
 import factory.random
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date
 
-faker.Faker().date_time_between
-faker.Faker().date_time()
+# faker.Faker().date_time_between
+# faker.Faker().date_time()
 
 from src.models.Booking import Booking
 from .AppCategoryFactory import AppCategoryFactory
@@ -12,8 +12,8 @@ class BookingFactory(factory.Factory[Booking]):
     class Meta:
         model = Booking
 
-    Start = factory.Faker("date_time")
+    Start = factory.Faker("date_time_between", start_date=date.today() - timedelta(days=10), end_date=date.today() + timedelta(days=10))
     # End = factory.LazyAttribute(lambda obj: obj.Start + timedelta(hours=factory.random.randgen.randint(1,3)))
-    End = factory.LazyAttribute(lambda obj: factory.Faker("date_time_between", start_date=obj.Start))
+    End = factory.LazyAttribute(lambda obj: faker.Faker().date_time_between(start_date=obj.Start, end_date=obj.Start + timedelta(hours=8)))
 
     Category = factory.SubFactory(AppCategoryFactory)
