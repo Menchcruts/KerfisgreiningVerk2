54d9b46ebf9e96ac5ff46c220abcc4c6c9971db7
Menchcruts <joelarnar@gmail.com>
2025-04-01 16:26:03 +0000

    Breytti data listum í dictionary frekar.

--- app.py
+++ app.py
@@ -10,13 +10,13 @@ from datetime import datetime, timedelta
 app = Flask(__name__)
 
 
-businesses = []
-users = []
-bookings = []
+businesses = {}
+users = {}
+bookings = {}
 
 @app.route('/register_user', methods=['POST'])
 def register_user():
-    data = request.json 
+    data = request.json
     name = data.get('name')
     email = data.get('email')
     
@@ -49,11 +49,7 @@ def add_appointment_category(business_id):
     if not name or lengthInMinutes is None or minNumOfHoursBeforeCancellation is None:
         abort(400, 'Name, lengthInMinutes and minNumOfHoursBeforeCancellation are required')
         
-    business = None
-    for b in business:
-        if str(b.id) == business_id:
-            business = b
-            break
+    business = businesses.get(str(business_id))
     if not business:
         abort(404, 'Business not found')
             
@@ -69,18 +65,14 @@ def book_appointment():
     appointment_category_id = data.get('appointment_category_id')
     scheduled_time = data.get('scheduled_time')
     
-    user = None
-    for u in users:
-        if str(u.id) == user_id:
-            user = u
-            break
-        
-    business = None
-    for b in business:
-        if str(b.id) == business_id:
-            business = b
-            break
-        
+    user = users.get(str(user_id))
+    if not user:
+        abort(404, "User not found.")
+
+    business = businesses.get(business_id)
+    if not business:
+        abort(404, "Business not found.")
+    
     appointment_category = None
     for ac in appointment_category:
         if str(ac.id) == appointment_category_id:
@@ -100,11 +92,7 @@ def book_appointment():
 
 @app.route('/get_user_bookings/<user_id>', methods=['GET'])
 def get_user_bookings(user_id):
-    user = None
-    for u in users:
-        if str(u.id) == user_id:
-            user = u
-            break
+    user = users.get(user_id)
     if not user:
         abort(404, 'User not found')
         
