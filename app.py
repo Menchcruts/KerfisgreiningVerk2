c308ccf6ed6b69a2e0adff36332e8e90e62552cc
richerdman <112634685+richerdman@users.noreply.github.com>
2025-04-02 16:19:02 +0000

    API Unittests for current app.py working and adjusted models to fit app and unittests

--- app.py
+++ app.py
@@ -23,92 +23,101 @@ def register_user():
     if not name or not email:
         abort(400, 'Name and email are required')
         
-    user = User(name=name, email=email)
-    users.append(user)
-    return jsonify({'message': 'User registered successfully', "user": str(user)}), 201
+    try:
+        new_user = User(name=name, email=email)
+    except ValueError as e:
+        return jsonify({'error': str(e)}), 400
+    return jsonify({'message': 'User registered successfully',
+                    'user': {
+                        'id': str(new_user.id),
+                        'name': new_user.name
+                        }
+                    }), 201
 
 @app.route('/create_business', methods=['POST'])
 def create_business():
     data = request.json 
-    name = data.get('name')
+    business_name = data.get('business_name')
     
-    if not name:
+    if not business_name:
         abort(400, 'Name is required')
         
-    business = Business(name=name)
-    businesses.append(business)
-    return jsonify({'message': 'Business created successfully', "business": str(business)}), 201
+    try:
+        new_business = Business(business_name=business_name)
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+    
+    return jsonify({'message': 'Business created successfully', 
+                    "business":{
+                        'id': str(new_business.id), 
+                        'business_name': new_business.business_name
+                        }
+                    }), 201
 
 @app.route('/add_appointment_category/<business_id>', methods=['POST'])
 def add_appointment_category(business_id):
     data = request.json 
-    name = data.get('name')
-    lengthInMinutes = data.get('lengthInMinutes')
-    minNumOfHoursBeforeCancellation = data.get('minNumOfHoursBeforeCancellation')
-    
-    if not name or lengthInMinutes is None or minNumOfHoursBeforeCancellation is None:
-        abort(400, 'Name, lengthInMinutes and minNumOfHoursBeforeCancellation are required')
-        
-    business = None
-    for b in business:
-        if str(b.id) == business_id:
-            business = b
-            break
+    category_name = data.get('category_name')
+    if not category_name:
+        print('Category name is required')
+        return jsonify({'error': 'Category name is required'}), 400
+
+    business = Business.get_business_by_id(str(business_id))
     if not business:
-        abort(404, 'Business not found')
-            
-    appointment_category = AppointmentCategory(name=name, lengthInMinutes=lengthInMinutes, minNumOfHoursBeforeCancellation=minNumOfHoursBeforeCancellation)
-    business.addAppointmentCategory(appointment_category)
-    return jsonify({'message': 'Appointment category added successfully', "appointment_category": str(appointment_category)}), 201
+        return jsonify({'error': 'Business not found'}), 404
+    
+    try:
+        new_category = AppointmentCategory(category_name=category_name, minNumOfHoursBeforeCancellation=data.get('minNumOfHoursBeforeCancellation'), lengthInMinutes=data.get('lengthInMinutes'))
+        business.addAppointmentCategory(new_category)
+    except ValueError as e:
+        print(f"Error: {e}")
+        return jsonify({'error': str(e)}), 400
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+    return jsonify({'message': 'Appointment category added successfully',
+                    'category': {
+                        'name': new_category.category_name,
+                        'id': str(new_category.id)
+                    }}), 201
 
 @app.route('/book_appointment', methods=['POST'])
 def book_appointment():
     data = request.json
-    user_id = data.get('user_id')
-    business_id = data.get('business_id')
-    appointment_category_id = data.get('appointment_category_id')
-    scheduled_time = data.get('scheduled_time')
+    user_id = data.get('userId')
+    business_id = data.get('businessId')
+    appointment_category_id = data.get('appointmentCategoryId')
+    scheduled_time = data.get('scheduledTime')
     
-    user = None
-    for u in users:
-        if str(u.id) == user_id:
-            user = u
-            break
+    if not user_id or not business_id or not appointment_category_id or not scheduled_time:
+        abort(400, 'User ID, business ID, appointment category ID and scheduled time are required')
+
+    try:
+        scheduled_time = datetime.fromisoformat(scheduled_time)
+    except ValueError:
+        abort(400, 'Invalid scheduled time format. Use ISO format (YYYY-MM-DDTHH:MM:SS)')
+    if scheduled_time < datetime.now():
+        abort(400, 'Scheduled time must be in the future') 
         
-    business = None
-    for b in business:
-        if str(b.id) == business_id:
-            business = b
-            break
+    appointment_category = AppointmentCategory.get_appointment_category_by_id(str(appointment_category_id))
+    if not appointment_category:
+        abort(404, 'Appointment category not found')
         
-    appointment_category = None
-    for ac in appointment_category:
-        if str(ac.id) == appointment_category_id:
-            appointment_category = ac
-            break
-    
-    if not user or not business or not appointment_category:
-        abort(400, 'User, business and appointment category are required')
-        
-    scheduled_time = datetime.fromisoformat(scheduled_time)
-    if not BookingService.isAvailableForBooking(business, appointment_category, scheduled_time):
-        abort(400, 'Time slot is not available')
-        
-    booking = Booking(Start = scheduled_time, End = scheduled_time + timedelta(minutes=appointment_category.lengthInMinutes), Category = appointment_category)
+    booking = Booking(user_id, Start = scheduled_time, End = scheduled_time + timedelta(minutes=appointment_category.lengthInMinutes), Category = appointment_category)
     bookings.append(booking)
-    return jsonify({'message': 'Appointment booked successfully', "booking": str(booking)}), 201
+    return jsonify({'message': 'Appointment booked successfully', "booking": str(booking), 'for user': str(user_id)}), 201
 
 @app.route('/get_user_bookings/<user_id>', methods=['GET'])
-def get_user_bookings(user_id):
-    user = None
-    for u in users:
-        if str(u.id) == user_id:
-            user = u
-            break
-    if not user:
-        abort(404, 'User not found')
-        
-    user_bookings = [str(booking) for booking in bookings if booking.scheduledStartTime >= datetime.now()]
+def get_user_bookings(user_id):    
+    user_bookings = [
+        {
+        'id': str(booking.id),
+        'scheduled_start_time': booking.scheduledStartTime,
+        'scheduled_end_time': booking.scheduledEndTime,
+        }
+        for booking in bookings if booking.user_id == user_id and booking.scheduledStartTime >= datetime.now()
+        ]
+    if not user_bookings:
+        return jsonify({'message': 'No upcoming bookings found'}), 404
     return jsonify({'bookings': user_bookings}), 200
 
 if __name__ == '__main__':
--- src/models/User.py
+++ src/models/User.py
@@ -6,6 +6,24 @@ class User:
     id: uuid.UUID = field(init=False, default_factory=uuid.uuid1)
     name: str
     email: str
+    
+    users = []
+    
+    def __post_init__(self):
+        if not isinstance(self.name, str) or not self.name:
+            raise ValueError(f"{self.name} is not a non empty str")
+        if not isinstance(self.email, str) or not self.email:
+            raise ValueError(f"{self.email} is not a non empty str")
+        if "@" not in self.email:
+            raise ValueError(f"{self.email} is not a valid email address")
+        
+    @classmethod
+    def get_user_by_id(self, user_id: str):
+        for user in User.users:
+            if str(user.id) == user_id:
+                return user
+        return None
+
 
     def __str__(self):
         return f"{self.name} - {self.email}"
